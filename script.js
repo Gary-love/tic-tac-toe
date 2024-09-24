2c308798715ae5ce343392629e9265a6dba75106
Gary-love <sigmapet086@gmail.com>
2024-09-24 13:56:09 +0300

    Add playGame() funcion

--- script.js
+++ script.js
@@ -16,3 +16,36 @@ function createPlayer(name,mark){
 }
 let player1=createPlayer("Player1","X")
 let player2=createPlayer("Player2","O")
+function playGame(player1,player2){
+    let round=1;
+    let game=gameBoard();
+    let board=game.getBoard();
+    while(round<=9){
+        if(round%2===0){
+            console.log("player 2 turn\nchoose the cell you  want")
+            let row=prompt("Enter cell number (1-9)")
+            let column=prompt("Enter cell number (1-9)")
+            board[row][column]="O"
+            round++;
+        }
+        else{
+            console.log("player 1 turn\nchoose the cell you  want")
+            let row=prompt("Enter cell number (1-9)")
+            let column=prompt("Enter cell number (1-9)")
+            board[row][column]="X"
+            round++;
+        }
+        let winner=determineWinner(player1,player2,board)
+        if(winner==="No winner"){
+          console.log("No winner yet")  
+        }
+        else{
+            console.log(winner)
+            round=10;
+        }
+         console.log(board)
+         console.log(round)
+         console.log(winner)
+    }
+}
+playGame(player1,player2)
